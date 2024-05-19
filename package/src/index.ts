3e265f24c870469979c01cb46900a9f4357a4a19
Colin Goodale <colingoodale@gmail.com>
2024-05-19 18:58:36 -0500

    finalizing exports

--- package/src/index.ts
+++ package/src/index.ts
@@ -8,7 +8,16 @@ export { default as DropdownSelect } from './components/DropdownSelect';
 export { default as NativeTextInput } from './components/NativeTextInput';
 export { default as Checkbox } from './components/Checkbox';
 
-//Hooks
+//Animation Hooks
+export { default as useBounce } from './hooks/useBounce';
 export { default as useFade } from './hooks/useFade';
+export { default as useRotate } from './hooks/useRotate';
 export { default as useSlide } from './hooks/useSlide';
 export { default as useStagger } from './hooks/useStagger';
+
+//Animation Components
+export { default as BounceView } from './components/BounceView';
+export { default as FadeView } from './components/FadeView';
+export { default as RotateView } from './components/RotateView';
+export { default as SlideView } from './components/SlideView';
+export { default as StaggerView } from './components/StaggerView';
